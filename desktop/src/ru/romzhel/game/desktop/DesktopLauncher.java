52cc03f95008137f0509d50c1adf5b3989f2f358
Roman Zheludkov <romzhel@mail.ru>
2021-07-27 13:36:51 +0300

    initial commit

---
+++ desktop/src/ru/romzhel/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ru.romzhel.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ru.romzhel.game.StarGame;
+
+public class DesktopLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
