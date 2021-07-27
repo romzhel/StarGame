29c8a717b931335cbb624204ac67b59c32c857ec
Roman Zheludkov <romzhel@mail.ru>
2021-07-27 15:08:47 +0300

    homework to lesson 1

--- desktop/src/ru/romzhel/game/desktop/DesktopLauncher.java
+++ desktop/src/ru/romzhel/game/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import ru.romzhel.game.StarGame;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
